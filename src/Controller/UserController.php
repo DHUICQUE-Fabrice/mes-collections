9b4fa4776618c93de6bda8df5f727a95883bb4d5
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2022-01-21 14:05:02 +0100

    Pagination page profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\HorseSchleichRepository;
+use App\Repository\PetshopRepository;
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,13 +17,28 @@ class UserController extends AbstractController
     /**
      * @Route("/profil/{nickname}", name="profile")
      */
-    public function profile(string $nickname, UserRepository $userRepository): Response
+    public function profile(string $nickname,
+                            UserRepository $userRepository,
+                            PetshopRepository $petshopRepository,
+                            HorseSchleichRepository $horseSchleichRepository,
+                            PaginatorInterface $paginator,
+                            Request $request
+                            ): Response
     {
-        $user = new User();
         $user = $userRepository->findOneBy(['nickname' => $nickname]);
+        $petshops = $petshopRepository->findBy(['user' => $user]);
+        $horseSchleiches = $horseSchleichRepository->findBy(['user' => $user]);
+
+        $rawItems = array_merge($petshops, $horseSchleiches);
+        $items = $paginator->paginate(
+            $rawItems,
+            $request->query->getInt('page', 1), 12
+        );
 
         return $this->render('user/profile.html.twig', [
             'user' => $user,
+            'items' => $items
         ]);
     }
 }
+
